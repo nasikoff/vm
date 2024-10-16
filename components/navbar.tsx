7fe12d182e36bfa987905ec40df78a4cdd5208d0
rizakhanoff <nf@rizakhanoff.com>
2024-10-16 23:16:03 +0300

    modified:   components/navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -58,32 +58,32 @@ export const Navbar = () => {
         <ThemeSwitch />
 
       </NavbarContent>
-
+      <NavbarContent  justify="center" className="w-full bg-transparent" style={{backdropFilter:"none"}}>
+      
+      <div  className="w-auto justify-evenly flex gap-4 justify-start ml-2 px-5 py-4 bg-content1  outline-none bg-content1 outline-none data-[focus-visible=true]:z-10 data-[focus-visible=true]:outline-2 data-[focus-visible=true]:outline-focus data-[focus-visible=true]:outline-offset-2 shadow-medium rounded-large transition-transform-background motion-reduce:transition-none">
+                {siteConfig.navItems.map((item) => (
+                  <NavbarItem className="flex justify-center w-[60px]" style={{backdropFilter:"none"}}>
+      
+                    <NextLink
+                      className={pathUrl === item.href
+                        ? "text-[#338EF7] content-navi "
+                        : "opacity-70 "} 
+                      color="foreground"
+                      href={item.href}
+      
+                    >
+                      <div className="">{item.svg}</div> 
+                    </NextLink>
+                  </NavbarItem>
+                ))}
+              </div>
+            </NavbarContent>
     </NextUINavbar>
     
-    <NextUINavbar maxWidth="xl" className="fixed bottom-5 top-[auto] bg-transparent" style={{backdropFilter:"none"}}>
+     
 
-<NavbarContent  justify="center" className="w-full bg-transparent" style={{backdropFilter:"none"}}>
-      
-<div  className="w-auto justify-evenly flex gap-4 justify-start ml-2 px-5 py-4 bg-content1  outline-none bg-content1 outline-none data-[focus-visible=true]:z-10 data-[focus-visible=true]:outline-2 data-[focus-visible=true]:outline-focus data-[focus-visible=true]:outline-offset-2 shadow-medium rounded-large transition-transform-background motion-reduce:transition-none">
-          {siteConfig.navItems.map((item) => (
-            <NavbarItem className="flex justify-center w-[60px]" style={{backdropFilter:"none"}}>
 
-              <NextLink
-                className={pathUrl === item.href
-                  ? "text-[#338EF7] content-navi "
-                  : "opacity-70 "} 
-                color="foreground"
-                href={item.href}
-
-              >
-                <div className="">{item.svg}</div> 
-              </NextLink>
-            </NavbarItem>
-          ))}
-        </div>
-      </NavbarContent>
-    </NextUINavbar>
+    
     
     </>
 
