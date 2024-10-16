15728f844d9434b07fd1cbb088f4ce9f06ea6bd1
rizakhanoff <nf@rizakhanoff.com>
2024-10-16 23:21:24 +0300

    modified:   components/navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,18 +2,28 @@ import {
   Navbar as NextUINavbar,
   NavbarContent,
  
+  NavbarMenuToggle,
   NavbarBrand,
   NavbarItem,
  
 } from "@nextui-org/navbar";
  
  
+import { Link } from "@nextui-org/link";
  
+import { link as linkStyles } from "@nextui-org/theme";
 import NextLink from "next/link";
- 
+import clsx from "clsx";
+import { usePathname } from "next/navigation";
 import { siteConfig } from "@/config/site";
 import { ThemeSwitch } from "@/components/theme-switch";
-import {Logo,
+import {
+  TwitterIcon,
+  GithubIcon,
+  DiscordIcon,
+ 
+ 
+  Logo,
 } from "@/components/icons";
 
 
@@ -50,14 +60,7 @@ export const Navbar = () => {
       </NavbarContent>
       <NavbarContent  justify="center" className="w-full bg-transparent" style={{backdropFilter:"none"}}>
       
-      <div  className="w-auto justify-evenly flex gap-4 justify-start ml-2 px-5 py-4 bg-content1  outline-none bg-content1 outline-none data-[focus-visible=true]:z-10 data-[focus-visible=true]:outline-2 data-[focus-visible=true]:outline-focus data-[focus-visible=true]:outline-offset-2 shadow-medium rounded-large transition-transform-background motion-reduce:transition-none">
-                {siteConfig.navItems.map((item) => (
-                  <NavbarItem className="flex justify-center w-[60px]" style={{backdropFilter:"none"}}>
-      
-                 
-                  </NavbarItem>
-                ))}
-              </div>
+   
             </NavbarContent>
     </NextUINavbar>
     
