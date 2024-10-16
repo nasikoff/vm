7aaa3e24679f158d49c86308f232207f0285a8ae
rizakhanoff <nf@rizakhanoff.com>
2024-10-16 23:12:18 +0300

    modified:   components/navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -14,7 +14,7 @@ import { Link } from "@nextui-org/link";
 import { link as linkStyles } from "@nextui-org/theme";
 import NextLink from "next/link";
 import clsx from "clsx";
-
+import { usePathname } from "next/navigation";
 import { siteConfig } from "@/config/site";
 import { ThemeSwitch } from "@/components/theme-switch";
 import {
@@ -26,54 +26,73 @@ import {
   Logo,
 } from "@/components/icons";
 
+
 export const Navbar = () => {
-  
+  const pathUrl = usePathname();
 
   return (
-    <NextUINavbar maxWidth="xl" position="sticky">
+    
+    <>
+    
+    
+    
+    <NextUINavbar maxWidth="xl" className="fixed top-0">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
         <NavbarBrand className="gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center gap-1" href="/">
             <Logo />
-            <p className="font-bold text-inherit">ACME</p>
+
           </NextLink>
         </NavbarBrand>
-        <div className="hidden lg:flex gap-4 justify-start ml-2">
+      </NavbarContent>
+
+      <NavbarContent className="hidden sm:flex basis-1/5 sm:basis-full" justify="end">
+        <NavbarItem className="hidden sm:flex gap-2">
+          <ThemeSwitch />
+        </NavbarItem>
+
+      </NavbarContent>
+
+      <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
+
+        <ThemeSwitch />
+
+      </NavbarContent>
+
+    </NextUINavbar>
+    
+    <NextUINavbar maxWidth="xl" className="fixed bottom-5 top-[auto] bg-transparent" style={{backdropFilter:"none"}}>
+
+<NavbarContent  justify="center" className="w-full bg-transparent" style={{backdropFilter:"none"}}>
+      
+<div  className="w-auto justify-evenly flex gap-4 justify-start ml-2 px-5 py-4 bg-content1  outline-none bg-content1 outline-none data-[focus-visible=true]:z-10 data-[focus-visible=true]:outline-2 data-[focus-visible=true]:outline-focus data-[focus-visible=true]:outline-offset-2 shadow-medium rounded-large transition-transform-background motion-reduce:transition-none">
           {siteConfig.navItems.map((item) => (
-            <NavbarItem key={item.href}>
+            <NavbarItem className="flex justify-center w-[60px]" style={{backdropFilter:"none"}}>
+
               <NextLink
-                className={clsx(
-                  linkStyles({ color: "foreground" }),
-                  "data-[active=true]:text-primary data-[active=true]:font-medium",
-                )}
+                className={pathUrl === item.href
+                  ? "text-[#338EF7] content-navi "
+                  : "opacity-70 "} 
                 color="foreground"
                 href={item.href}
+
               >
-                {item.label}
+                <div className="">{item.svg}</div> 
               </NextLink>
             </NavbarItem>
           ))}
         </div>
       </NavbarContent>
-
-      <NavbarContent
-        className="hidden sm:flex basis-1/5 sm:basis-full"
-        justify="end"
-      >
-        <NavbarItem className="hidden sm:flex gap-2">
-      
-          <ThemeSwitch />
-        </NavbarItem>
-         
-   
-      </NavbarContent>
-
-      <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
- 
-        <ThemeSwitch />
-        <NavbarMenuToggle />
-      </NavbarContent>
- 
     </NextUINavbar>
+    
+    </>
+
+
+
+
+
+
+
+
   );
 };
