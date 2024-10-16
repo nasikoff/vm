1280cabd6e32866b4d37dad6f8757e5aee0ec3c9
rizakhanoff <nf@rizakhanoff.com>
2024-10-16 22:34:12 +0300

    modified:   components/navbar.tsx 	deleted:    config/site.ts 	new file:   config/site.tsx 	modified:   layouts/default.tsx 	modified:   layouts/head.tsx 	modified:   package.json 	modified:   pages/about/index.tsx 	deleted:    pages/api/hello.ts 	modified:   pages/index.tsx 	modified:   styles/globals.css

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -5,8 +5,9 @@ export default function DocsPage() {
   return (
     <DefaultLayout>
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-        <div className="inline-block max-w-lg text-center justify-center">
-          <h1 className={title()}>About</h1>
+      <div className="inline-block max-w-xl text-center justify-center">
+          <span className={title()}>Make&nbsp;</span>
+          <span className={title({ color: "violet" })}>about&nbsp;</span>
         </div>
       </section>
     </DefaultLayout>
