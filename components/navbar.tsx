46e09aeca8bfc53cb36b9d697cd106537ec88600
rizakhanoff <nf@rizakhanoff.com>
2024-10-17 00:01:19 +0300

    modified:   components/navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -25,28 +25,11 @@ import {
   SearchIcon,
   Logo,
 } from "@/components/icons";
+import { usePathname } from "next/navigation";
 
 export const Navbar = () => {
-  const searchInput = (
-    <Input
-      aria-label="Search"
-      classNames={{
-        inputWrapper: "bg-default-100",
-        input: "text-sm",
-      }}
-      endContent={
-        <Kbd className="hidden lg:inline-block" keys={["command"]}>
-          K
-        </Kbd>
-      }
-      labelPlacement="outside"
-      placeholder="Search..."
-      startContent={
-        <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-      }
-      type="search"
-    />
-  );
+  const pathUrl = usePathname();
+ 
 
   return (
 
