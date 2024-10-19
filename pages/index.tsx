0ada6c7494b47333ef234f3af0a8b5f7e07a0fac
rizakhanoff <nf@rizakhanoff.com>
2024-10-19 21:46:20 +0300

    modified:   pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -311,8 +311,7 @@ return (
           
      
       <div className="flex-auto w-full md:w-64">
-      <iframe className="rounded-[20px] h-[300px] md:h-[450px]" src="https://yandex.ru/map-widget/v1/?um=constructor%3A6335f8acd1220368f602308808ddf0c3bfb498b2900e9700412d6e8337c173b6&amp;source=constructor" width="100%"  />
-
+ 
       </div>
           
           
